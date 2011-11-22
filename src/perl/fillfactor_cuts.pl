507fdb5689fdf77fc54492c4b76f04813b95c969
Heinrich Schmidt <heinrich.schmidt@tu-bs.de>
2011-11-22 14:49:06 +0100

    adding some latex-documentation files

--- src/perl/fillfactor_cuts.pl
+++ src/perl/fillfactor_cuts.pl
@@ -4,13 +4,13 @@ if(-e $ARGV[0]){
 	open(fhz,">","z.dat");
 	open(fhy,">","y.dat");
 	open(fhx,">","x.dat");
-	for($i=0;$i<20;$i+=0.1)
+	for($i=0;$i<100;$i+=1)
 	{
-		$tmp = `perl subfillingfactor.pl ../cpp/rbd05.data 0 0 1 $i 1 400`;
+		$tmp = `perl subfillingfactor.pl $ARGV[0] 0 0 1 $i 2 20000`;
 		print fhz "$i\t$tmp";
-		$tmp = `perl subfillingfactor.pl ../cpp/rbd05.data 0 1 0 $i 1 400`;
+		$tmp = `perl subfillingfactor.pl $ARGV[0] 0 1 0 $i 2 20000`;
 		print fhy "$i\t$tmp";
-		$tmp = `perl subfillingfactor.pl ../cpp/rbd05.data 1 0 0 $i 1 400`;
+		$tmp = `perl subfillingfactor.pl $ARGV[0] 1 0 0 $i 2 20000`;
 		print fhx "$i\t$tmp";
 	}
 	close(fhz);
--- src/perl/subfillingfactor.pl
+++ src/perl/subfillingfactor.pl
@@ -14,14 +14,14 @@ if(-e $ARGV[0])
 	open(fh,"<",$ARGV[0]);
 		@file = <fh>;
 	close(fh);
-	$fvolume = 0;
+	$fvolume = 0;#initialize the variable so it can be used to add things to it
 	foreach(@file)
 	{
 		chomp();
-		@xyzr = split/,/;
+		@xyzr = split/,/; #splits the line of the file into a list
 #		print "$xyzr[2]\n";
-		$hess = ( ($x*$xyzr[0])	+($y*$xyzr[1]) 	+ ($z*$xyzr[2]) - $d );
-		if(($hess>=0)and($hess <= $w))
+		$hess = ( ($x*$xyzr[0])	+($y*$xyzr[1]) 	+ ($z*$xyzr[2]) - $d ); #calculate the hessian normalform of the shpere to test its position
+		if(($hess>=0)and($hess <= $w))#if the shpere is within the borders, add its value to the filling factor
 		{
 			$fvolume += $xyzr[3]**3 * 4/3 * $pi;
 		}
